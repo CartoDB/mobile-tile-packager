0a5cef09b14bdc4fb0cdec96a8c61ffe6c108767
Jaak <jaak@nutiteq.com>
2017-08-10 12:21:54 +0300

    test

--- test/unit/named_maps_api.js
+++ test/unit/named_maps_api.js
@@ -1,47 +1,49 @@
 'use strict';
 
-var fs = require('fs'); 
+var fs = require('fs');
 var conf = require('../../config.js');
 var api = require('../../api.js');
 var hlprs = require('../../helpers.js');
 var assert = require('assert');
 var params = require('../params.js');
 
-describe('Named Maps API test...', function() { 
-  before(function() {
-    if (hlprs.fileExists(params.options.tilesfileMapsAPI)) {
-      fs.unlink(params.options.tilesfileMapsAPI, function(err){
-        if(err) throw err;
-      });
-    }
-  });
-  
-  api.inst(params.options.username, params.options.template, function(err, layerId, meta){
-    if (err) cb(err);      
-    else {
-        var tileurl = conf.tile_url.replace('{username}', params.options.username).replace('{layergroupid}', layerId);
-  
-        it('should download tiles and create ' + params.options.tilesfileMapsAPI + ' file', function(cb) {      
-          api.getMbtiles(tileurl, params.options.tilesfileMapsAPI, params.options.minzoom, params.options.maxzoom, params.options.bounds, params.options.logOutput, function(err){
-            if (err) cb(err);      
-            else cb();
-          });
+describe('Named Maps API test...', function() {
+    before(function() {
+        if (hlprs.fileExists(params.options.tilesfileMapsAPI)) {
+            fs.unlink(params.options.tilesfileMapsAPI, function(err) {
+                if (err) throw err;
+            });
+        }
+    });
+
+    it('get layergroupid first from init', function(cb) {
+        api.inst(params.options.username, params.options.template, function(err, layerId, meta) {
+            if (err) cb(err);
+            else {
+                var tileurl = conf.tile_url.replace('{username}', params.options.username).replace('{layergroupid}', layerId);
+
+                it('should download tiles and create ' + params.options.tilesfileMapsAPI + ' file', function(cb) {
+                    api.getMbtiles(tileurl, params.options.tilesfileMapsAPI, params.options.minzoom, params.options.maxzoom, params.options.bounds, params.options.logOutput, function(err) {
+                        if (err) cb(err);
+                        else cb();
+                    });
+                });
+
+                it('file ' + params.options.tilesfileMapsAPI + ' should exsits and greater than 0 bytes', function() {
+                    assert.notEqual(hlprs.fileExists(params.options.tilesfileMapsAPI), 0);
+                });
+                cb();
+            }
         });
-    
-        it('file ' + params.options.tilesfileMapsAPI + ' should exsits and greater than 0 bytes', function() {      
-          assert.notEqual(hlprs.fileExists(params.options.tilesfileMapsAPI), 0);
-        });
-      cb();  
-    }
-  });
-  
-  
-  after(function() {
-    if (hlprs.fileExists(params.options.tilesfileMapsAPI)) {
-      fs.unlink(params.options.tilesfileMapsAPI, function(err){
-        if(err) throw err;
-      });
-    }
-  });
-  
+    });
+
+
+    after(function() {
+        if (hlprs.fileExists(params.options.tilesfileMapsAPI)) {
+            fs.unlink(params.options.tilesfileMapsAPI, function(err) {
+                if (err) throw err;
+            });
+        }
+    });
+
 });
