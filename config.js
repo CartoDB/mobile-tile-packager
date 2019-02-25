9dff835967df14cbb43c138e61021cb90d324e53
Eduardo Fernández León <eduardo.fernandez@geographica.gs>
2019-02-25 18:19:34 +0100

    Added server host configurable via config.js and docker-compose.yml instead of using localhost as hardcoded

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   server_port: port,
   inst_url: 'https://{username}.carto.com/api/v1/map/named/{template}',
   tile_url: 'https://{username}.carto.com/api/v1/map/{layergroupid}/{z}/{x}/{y}.mvt',
-  download_url: 'http://localhost:' + port + '/api/v1/package/get/{username}/{id}',
+  download_url: 'http://' + (process.env.MTP_HOST !== undefined ? process.env.MTP_HOST : 'localhost') + ':' + port + '/api/v1/package/get/{username}/{id}',
   sql_api_url: 'http://{username}.carto.com/api/v2/sql?q={sql}&format=GeoJSON',
   tippe: 'tippecanoe -o {mbtiles_file} --minimum-zoom={minzoom} --maximum-zoom={maxzoom} --layer=layer {geojson_file}',
   /* kue job manager settings */
